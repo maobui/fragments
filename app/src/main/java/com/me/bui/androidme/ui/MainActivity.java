0f39317a0ef42a46da48ad357094e14df9da454a
mao.buidinh <mao.buidinh@hotmail.com>
2018-07-06 18:18:23 +0700

    - design 2 panes for tablet.

--- app/src/main/java/com/me/bui/androidme/ui/MainActivity.java
+++ app/src/main/java/com/me/bui/androidme/ui/MainActivity.java
@@ -1,25 +1,69 @@
 package com.me.bui.androidme.ui;
 
 import android.content.Intent;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.GridView;
 import android.widget.Toast;
 
 import com.me.bui.androidme.R;
+import com.me.bui.androidme.data.ImageAssets;
 
 public class MainActivity extends AppCompatActivity implements MasterListFragment.OnImageClickListener {
 
     private int headIndex;
     private int bodyIndex;
     private int legIndex;
+
+    private boolean mTwoPane;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (findViewById(R.id.android_me_linear_layout) != null) {
+            mTwoPane = true;
 
+            Button nextButton = findViewById(R.id.next_btn);
+            nextButton.setVisibility(View.GONE);
+
+            GridView gridView = findViewById(R.id.images_grid_view);
+            gridView.setNumColumns(2);
+
+            if (savedInstanceState == null) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+
+                // Head
+                BodyPartFragment headFragment = new BodyPartFragment();
+                headFragment.setImageIds(ImageAssets.getHeads());
+
+                fragmentManager.beginTransaction()
+                        .add(R.id.head_container, headFragment)
+                        .commit();
+
+                // Body
+                BodyPartFragment bodyFragment = new BodyPartFragment();
+                bodyFragment.setImageIds(ImageAssets.getBodies());
+
+                fragmentManager.beginTransaction()
+                        .add(R.id.body_container, bodyFragment)
+                        .commit();
+
+                // Leg
+                BodyPartFragment legFragment = new BodyPartFragment();
+                legFragment.setImageIds(ImageAssets.getLegs());
+
+                fragmentManager.beginTransaction()
+                        .add(R.id.leg_container, legFragment)
+                        .commit();
+            }
+        } else {
+            mTwoPane = false;
+        }
     }
 
     @Override
@@ -28,6 +72,34 @@ public class MainActivity extends AppCompatActivity implements MasterListFragmen
 
         int bodyPartNumber = position/12;
         int listIndex = position - 12*bodyPartNumber;
+
+        if(mTwoPane) {
+            BodyPartFragment fragment = new BodyPartFragment();
+            switch (bodyPartNumber) {
+                case 0:
+                    fragment.setImageIds(ImageAssets.getHeads());
+                    fragment.setListIndex(listIndex);
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.head_container, fragment)
+                            .commit();
+                    break;
+                case 1:
+                    fragment.setImageIds(ImageAssets.getBodies());
+                    fragment.setListIndex(listIndex);
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.body_container, fragment)
+                            .commit();
+                    break;
+                case 2:
+                    fragment.setImageIds(ImageAssets.getLegs());
+                    fragment.setListIndex(listIndex);
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.leg_container, fragment)
+                            .commit();
+                    break;
+            }
+        }
+
         switch (bodyPartNumber) {
             case 0 :
                 headIndex = listIndex;
