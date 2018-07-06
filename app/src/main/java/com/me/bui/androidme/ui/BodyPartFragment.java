6fbe99682786f2076e3ecf99f8b10892e6f919bb
mao.buidinh <mao.buidinh@hotmail.com>
2018-07-06 16:36:31 +0700

    - basic fragment.

---
+++ app/src/main/java/com/me/bui/androidme/ui/BodyPartFragment.java
@@ -0,0 +1,50 @@
+package com.me.bui.androidme.ui;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.me.bui.androidme.R;
+import com.me.bui.androidme.data.ImageAssets;
+
+import java.util.List;
+
+/**
+ * Created by mao.bui on 7/6/2018.
+ */
+public class BodyPartFragment extends Fragment {
+
+    private static final String TAG = BodyPartFragment.class.getSimpleName();
+    private List<Integer> mImageIds;
+    private int mListIndex;
+
+    public BodyPartFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_body_part, container, false);
+        ImageView imageView = root.findViewById(R.id.body_part_imgv);
+        if (mImageIds != null) {
+            imageView.setImageResource(mImageIds.get(mListIndex));
+        } else {
+            Log.v(TAG, "This fragment has a null list of image id's.");
+        }
+        return root;
+    }
+
+    public void setImageIds(List<Integer> imageIds) {
+        mImageIds = imageIds;
+    }
+
+    public void setListIndex(int listIndex) {
+        mListIndex = listIndex;
+    }
+}
--- app/src/main/java/com/me/bui/androidme/ui/MainActivity.java
+++ app/src/main/java/com/me/bui/androidme/ui/MainActivity.java
@@ -1,9 +1,11 @@
 package com.me.bui.androidme.ui;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.me.bui.androidme.R;
+import com.me.bui.androidme.data.ImageAssets;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BodyPartFragment headFragment = new BodyPartFragment();
+        headFragment.setImageIds(ImageAssets.getHeads());
+        headFragment.setListIndex(5);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .add(R.id.head_container, headFragment)
+                .commit();
     }
 }
